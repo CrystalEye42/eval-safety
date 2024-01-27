ae92e7f7187a31cbcf4fa9bfca81d67500d57956
irugina1 <ileana.rugina.2@gmail.com>
2024-01-27 11:52:44 -0500

    iter on ppl script

--- ppl.py
+++ ppl.py
@@ -1,16 +1,16 @@
+import argparse
+from ruamel.yaml import YAML
 import json
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import pickle
 import torch
+yaml = YAML(typ='safe')
+yaml.preserve_quotes = True
+yaml.default_flow_style = False
+yaml.allow_unicode = True
 
 
 DEVICE = torch.device("mps")
-model = AutoModelForCausalLM.from_pretrained("./Llama-2-7b-chat-hf").to(DEVICE)
-tokenizer = AutoTokenizer.from_pretrained(
-        'NousResearch/Llama-2-7b-chat-hf', cache_dir='llm_weights', use_fast=True)
-
-with open("malicious_task_maping_unstructured_30_llama-2.json") as f:
-    data = json.load(f)
 
 
 def get_ppl(prompt, model, tokenizer):
@@ -23,21 +23,47 @@ def get_ppl(prompt, model, tokenizer):
         return {'loss': loss.item(), 'num_tokens': inputs.shape[1]}
 
 
-result = {}
-for jailbreak_method, category_data in data.items():
-    print(f"jailbreak method = {jailbreak_method}", flush=True)
-    result[jailbreak_method] = {}
-    for category_name, subcategory_data in category_data.items():
-        result[jailbreak_method][category_name] = {}
-        for task_name, task_data in subcategory_data.items():
-            result[jailbreak_method][category_name][task_name] = {}
-            for severity_name, examples in task_data.items():
-                result[jailbreak_method][category_name][task_name][severity_name] = []
-                for example_idx, prompts in enumerate(examples):
-                    prompt = prompts["jailbreaking_prompt"]
-                    ppl_result = get_ppl(prompt, model, tokenizer)
-                    result[jailbreak_method][category_name][
-                            task_name][severity_name].append(ppl_result)
-# pickle is bad practice but this will do for now
-with open('ppl.pkl', 'wb') as file:
-    pickle.dump(result, file)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, help='LLaMA model')
+    parser.add_argument('--save_to', type=str, help='Output File Name')
+    parser.add_argument('--eval_malicious', action="store_true")
+    parser.add_argument('--eval_benign', action="store_true")
+    args = parser.parse_args()
+
+    model = AutoModelForCausalLM.from_pretrained(args.model).to(DEVICE)
+    tokenizer = AutoTokenizer.from_pretrained(
+            'NousResearch/Llama-2-7b-chat-hf', cache_dir='llm_weights', use_fast=True)
+
+    result = {}
+    if args.eval_malicious:
+        with open("malicious_task_maping_unstructured_30_llama-2.json") as f:
+            data = json.load(f)
+
+        for jailbreak_method, category_data in data.items():
+            print(f"jailbreak method = {jailbreak_method}", flush=True)
+            result[jailbreak_method] = {}
+            for category_name, subcategory_data in category_data.items():
+                result[jailbreak_method][category_name] = {}
+                for task_name, task_data in subcategory_data.items():
+                    result[jailbreak_method][category_name][task_name] = {}
+                    for severity_name, examples in task_data.items():
+                        result[jailbreak_method][category_name][task_name][severity_name] = []
+                        for example_idx, prompts in enumerate(examples):
+                            prompt = prompts["jailbreaking_prompt"]
+                            ppl_result = get_ppl(prompt, model, tokenizer)
+                            result[jailbreak_method][category_name][
+                                    task_name][severity_name].append(ppl_result)
+    if args.eval_benign:
+        with open("benign_integrated.yaml") as f:
+            data = yaml.load(f)
+        for jailbreak_method, examples in data.items():
+            print(f"jailbreak method = {jailbreak_method}", flush=True)
+            result[jailbreak_method] = []
+            for prompt in examples:
+                ppl_result = get_ppl(prompt, model, tokenizer)
+                result[jailbreak_method].append(ppl_result)
+
+    # pickle is bad practice but this will do for now
+    with open(args.save_to, 'wb') as file:
+        pickle.dump(result, file)
